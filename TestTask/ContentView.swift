5b62dbe81dc6c3b89a5fc705eb67bbbaa1ebb172
kiri4rr <kirill.romanenko2000@gmail.com>
2023-05-18 22:00:47 +0300

    Initial Commit

---
+++ TestTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTask
+//
+//  Created by Kirill Romanenko on 18.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
